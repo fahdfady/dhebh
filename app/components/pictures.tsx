294c60e04a746bc181e3182d63471292a6e7072d
fahdfady <fahd.fady212@gmail.com>
2024-01-26 00:30:16 +0200

    sections added 80%

--- app/components/pictures.tsx
+++ app/components/pictures.tsx
@@ -1,10 +1,18 @@
 import ImgCard from "./ImgCard";
-import { Carousel } from "./ui/carousel";
+
+import Image from "next/image";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/app/components/ui/carousel";
 
 export default function Pictures() {
     return (
         <section>
-            <div className="flex justify-center items-center gap-x-11 gap-y-11 px-8 pt-20 sm:px-0">
+            <div className="flex flex-wrap justify-center items-center gap-x-11 gap-y-11 px-8 pt-20 sm:px-0">
                 <div className="grid grid-cols-2 gap-6">
                     <ImgCard />
                     <ImgCard />
@@ -15,6 +23,7 @@ export default function Pictures() {
                 </div>
 
                 <Carousel
+                    className="relative h-[31rem] w-full max-w-[46rem] shrink-0 grow overflow-hidden rounded-2xl border-2 border-[#1c343e]"
                     opts={{
                         loop: true
                     }}
@@ -22,27 +31,30 @@ export default function Pictures() {
                     <CarouselContent>
                         <CarouselItem>
                             <Image
-                                src={'/banner.webp'}
-                                width={1700}
-                                height={600}
+                                src={'/outside-3.webp'}
+                                className="h-full w-full rounded-2xl object-cover duration-1000"
+                                width={500}
+                                height={500}
                                 alt="banner image"
                                 quality={70}
                             />
                         </CarouselItem>
                         <CarouselItem>
                             <Image
-                                src={'/banner.webp'}
-                                width={1700}
-                                height={600}
+                                src={'/outside-3.webp'}
+                                className="h-full w-full rounded-2xl object-cover duration-1000"
+                                width={500}
+                                height={500}
                                 alt="banner image"
                                 quality={70}
                             />
                         </CarouselItem>
                         <CarouselItem>
                             <Image
-                                src={'/banner.webp'}
-                                width={1700}
-                                height={600}
+                                src={'/outside-3.webp'}
+                                className="h-full w-full rounded-2xl object-cover duration-1000"
+                                width={500}
+                                height={500}
                                 alt="banner image"
                                 quality={70}
                             />
